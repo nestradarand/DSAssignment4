0441f58daae2f3b133f37a42e6fcfdd99a7d3441
nestradarand <noah.estrada456@gmail.com>
2019-10-30 20:23:23 +0000

    changes made to complete the metrics helper

--- MetricsHelper.cpp
+++ MetricsHelper.cpp
@@ -7,35 +7,68 @@ MetricsHelper::MetricsHelper()
 {
 
 }
-MetricsHelper::MetricsHelper(int *array[],int arraySize)
+//takes in an array by value to avoid pointer problems
+MetricsHelper::MetricsHelper(int array[],int arraySize)
 {
     baseDataSet = array;
     sizeOfArray = arraySize;
-    sort(baseDataSet,sizeOfArray);
+    sort();
 }
 MetricsHelper::~MetricsHelper()
 {
     delete [] baseDataSet;
 }
-void MetricsHelper::setNewDataset(int *array[], int arraySize)
+//takes in an array by value and its size
+void MetricsHelper::setNewDataset(int array[], int arraySize)
 {
     baseDataSet = array;
     sizeOfArray = arraySize;
-    sort(baseDataSet,sizeOfArray);
+    sort();
 }
 float MetricsHelper::getMean()
 {
-    
+    int sum = 0;
+    for(int i =0; i <sizeOfArray;++i)
+    {
+        sum = sum + baseDataSet[i];
+        cout << sum <<endl;
+    }
+    return (float(sum)/float(sizeOfArray));
 }
 float MetricsHelper::getMedian()
 {
-
+    int medianPosition = (sizeOfArray + 1) / 2;
+    if(sizeOfArray %2 != 0)        
+        return baseDataSet[medianPosition - 1]; //use minus one to account for base zero
+    else
+        return(float(baseDataSet[medianPosition-1]) +
+        float(float(baseDataSet[medianPosition])-float(baseDataSet[medianPosition-1]))/2);
+    
+    
+    
 }
 int MetricsHelper::getMax()
 {
-
+    return baseDataSet[sizeOfArray-1];
 }
-void MetricsHelper::sort(int *array[],int arraySize)
+void MetricsHelper::sort()//puts stored array of values in ascending order
 {
-
+    for(int i = 0; i < sizeOfArray; ++i)//for all numbers in the array
+    {
+        for(int j = (i + 1); j < sizeOfArray; ++j)//looks at the next value in line
+        {
+            if(baseDataSet[i] > baseDataSet[j])
+            {
+                //have both values switch spots if next value in line is less than the current position
+                int a = baseDataSet[i];
+                baseDataSet[i] = baseDataSet[j];
+                baseDataSet[j] = a;
+            }
+        }
+    }
+}
+void MetricsHelper::printArray()
+{
+    for(int i =0; i <sizeOfArray;++i)
+        cout << baseDataSet[i] <<endl;
 }
--- MetricsHelper.h
+++ MetricsHelper.h
@@ -5,22 +5,18 @@ class MetricsHelper
 {
     public:
         MetricsHelper();
-        MetricsHelper(int *array[],int arraySize);
+        MetricsHelper(int array[],int arraySize);
         ~MetricsHelper();
         //updates the data to get all the following metrics from
-        void setNewDataset(int *array[],int arraySize);
+        void setNewDataset(int array[],int arraySize);
         //returns the summary statistics for the data stored as an array in this instance
         float getMean();
         float getMedian();
         int getMax();
-
-
-
-
+        void printArray();
     private:
-        void sort(int *array[],int arraySize);
-        int **baseDataSet;
+        void sort();
+        int *baseDataSet;
         int sizeOfArray;
-        bool hasBeenSorted =false;
 };
 #endif
